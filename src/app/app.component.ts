a551b3f5c57138d175f0cf1087cbbcb0827b272c
ManalChailou <chailou.manal@gmail.com>
2023-10-13 17:33:21 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Personne} from "./classes/personne";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBouskri';
+  title = 'Angular Tp';
+  updateTitle(event: any) {
+    this.title = event.target.value;
+  }
+  personne: Personne=new Personne(100,'Sam','John');
+  tab: number[] = [2,5,8];
+  constructor() {}
+  direBonjour(){
+    return "Bonjour Angular";
+  }
 }
